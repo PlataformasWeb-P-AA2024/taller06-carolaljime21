4cecd17c4e867500c05e2c0d398b49b14a68dc9e
carolaljime21 <carolalvaradojime@gmail.com>
2024-05-17 17:58:20 -0500

    Paises

---
+++ ejercicio-02/genera_base.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+
+# se genera en enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine('sqlite:///basepaises.db')
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String
+
+class Pais(Base):
+    __tablename__ = 'paises'
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    capital = Column(String)
+    continente = Column(String)
+    dial = Column(String)
+    geonameID = Column(Integer)
+    itu = Column(String)
+    lenguajes = Column(String)
+    esIndependiente = Column(String)
+
+Base.metadata.create_all(engine)
+
---
+++ ejercicio-02/ingresa_data.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import requests
+from genera_base import Pais
+
+import json
+
+# se genera en enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine('sqlite:///basepersonas.db')
+
+url = "https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/616b1fb83cbfd4eb6d9e7d52924bb00a/country-codes_json.json"
+response = requests.get(url)
+data = response.json()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+for d in data:
+    pais = Pais(
+        nombrePais=d['CLDR display name'],
+        capital=d['Capital'],
+        continente=d['Continent'],
+        dial=d['Dial'],
+        geonameID=d['Geoname ID'],
+        itu=d['ITU'],
+        lenguajes=d['Languages'],
+        esIndependiente=d['is_independent']
+    )
+    session.add(pais)
+
+"""for d in data:
+    print( "%s %s %s %s %d %s %s %s"      % (d["CLDR display name"], 
+d["Capital"],
+        d["Continent"],
+        d["Dial"],
+        d["Geoname ID"],
+        d["ITU"],
+        d["Languages"],
+        d["is_independent"]))"""
+
+
+session.commit()
+session.close()
